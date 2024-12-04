eda57d73fba3dfab0ff947386a3238e77ed3d42f
Stefan Secrieru <secrieru23022@gmail.com>
2024-12-04 19:07:49 +0200

    Changes in the Rice and Sturges number of bins

--- phase2_tests.py
+++ phase2_tests.py
@@ -9,7 +9,7 @@ from test_importer import prepare_tests
 from utils.plots import plot_times
 from utils.statistics import bootstrap_resampling, analyze_metrics
 
-sudoku_puzzles = prepare_tests(0.0, 1.0)
+sudoku_puzzles = prepare_tests(0.0, 1.0, 5)
 metrics, times = sudoku_performance_all_methods(solve_sudoku_sat, sudoku_puzzles, 10, 2)
 
 analyze_metrics(times)
--- utils/plots.py
+++ utils/plots.py
@@ -4,7 +4,11 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 def plot_times(times):
-    sns.histplot(times, kde=True, bins=int(math.log(len(times), 2) + 1), color='skyblue', edgecolor='black')
+    if len(times) <= 200:
+        no_bins = int(math.log(len(times), 2) + 1)
+    else:
+        no_bins = int(2 * (len(times) ** (1/3)) )
+    sns.histplot(times, kde=True, bins=no_bins, color='skyblue', edgecolor='black')
     plt.xlabel('Solving time (seconds)')
     plt.ylabel('Frequency')
     plt.title('Solving time distribution for SAT')
